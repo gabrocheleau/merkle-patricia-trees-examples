33e285b3384dea79184b6a660e838f8c56b806d6
Gabriel Rocheleau <gab_rocheleau@hotmail.com>
2020-06-21 23:22:49 -0400

    Update example4b.js

--- examples/example4b.js
+++ examples/example4b.js
@@ -9,7 +9,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(INFURIA_ENDPOINT));
 
 async function createTransactionHash(transactionHash) {
   let transaction = await web3.eth.getTransaction(transactionHash);
-  transactionNode = [
+  transactionData = [
     web3.utils.toHex(transaction.nonce),
     web3.utils.toHex(transaction.gasPrice),
     web3.utils.toHex(transaction.gas),
@@ -20,8 +20,8 @@ async function createTransactionHash(transactionHash) {
     transaction.r,
     transaction.s,
   ];
-  console.log("Transaction data: ", transactionNode);
-  console.log("Transaction hash: ", keccak256(rlp.encode(transactionNode)));
+  console.log("Transaction data: ", transactionData);
+  console.log("Transaction hash: ", keccak256(rlp.encode(transactionData)));
 }
 createTransactionHash(
   "0x2f81c59fb4f0c3146483e72c1315833af79b6ea9323b647101645dc7ebe04074"
